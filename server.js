0c7dce75f5b6434b95dcedb4d32603c7d1c6e176
todd1terry1 <84549472+todd1terry1@users.noreply.github.com>
2022-04-20 11:07:24 -0500

    connected to express

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
+const mysql = require('mysql2');
+
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
-app.use (express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.listen(PORT, () => {
- console.log(`Server running on port ${PORT}`)
-});
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'toddterry',
+      // Your MySQL password
+      password: 'Sircharles#1',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
-app.get('/', (req, res)=>{
-    res.json({
-        mesage: 'Hello World'
-    });
-});
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
   });
+
+// Default response for any other request (Not Found) catchall
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
